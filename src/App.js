90929850e601c1d02cc36f75181ceb5e2fadf08f
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2019-01-28 17:00:35 +0000

    Enable custom plugin

--- src/App.js
+++ src/App.js
@@ -2,12 +2,50 @@ import React, { Component } from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg'
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Emoji from '@wwalc/ckeditor5-emoji/src/emoji';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';;
+
+
+class InsertImage extends Plugin {
+    init() {
+        const editor = this.editor;
+
+        editor.ui.componentFactory.add( 'insertImage', locale => {
+            const view = new ButtonView( locale );
+
+            view.set( {
+                label: 'Insert image',
+                icon: imageIcon,
+                tooltip: true
+            } );
+
+            // Callback executed once the image is clicked.
+            view.on( 'execute', () => {
+                const imageUrl = prompt( 'Image URL' );
+
+                editor.model.change( writer => {
+                    const imageElement = writer.createElement( 'image', {
+                        src: imageUrl
+                    } );
+
+                    // Insert the image in the current selection location.
+                    editor.model.insertContent( imageElement, editor.model.document.selection );
+                } );
+            } );
+
+            return view;
+        } );
+    }
+}
 
 class App extends Component {
   render() {
@@ -18,11 +56,11 @@ class App extends Component {
           onInit={ editor => console.log( 'Editor is ready to use!', editor ) }
           onChange={ ( event, editor ) => console.log( { event, editor } ) }
           config={ {
-              plugins: [ Essentials, Paragraph, Bold, Italic, Heading, Emoji ],
-              toolbar: [ 'heading', '|', 'bold', 'italic', '|', 'undo', 'redo', '|', 'emoji' ]
+            plugins: [Essentials, Paragraph, Bold, Italic, Image, InsertImage, ImageCaption],
+            toolbar: ['bold', 'italic', 'insertImage']
           } }
           editor={ ClassicEditor }
-          data="<p>Hello from CKEditor 5!</p>"
+          data=''
         />
       </div>
     );
